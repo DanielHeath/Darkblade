5e2a58b8f5c43cf9be06afbfe7cf3f279263975f
Daniel Heath <daniel.r.heath@gmail.com>
2009-04-29 19:12:31 +1000

    first commit

---
+++ DarkBlade/dbFoundation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLiteWrapper;
+
+namespace DarkBlade
+{
+    class dbFoundation
+    {
+        public dbFoundation()
+        {
+            // Creates a new instance of SQLiteBase and opens database in file "test.db"
+
+            SQLiteBase db = new SQLiteBase("Darkblade.db");
+            // executes SELECT query and store results in new data table
+
+            DataTable table = db.ExecuteQuery("SELECT * FROM table1 WHERE id = 1;");
+            // closes the database
+
+            db.CloseDatabase();
+        }
+    }
+}
