0274dc3a42c4da1dbb251e4cf7ef55282ff46280
Daniel Heath <daniel.r.heath@gmail.com>
2009-08-16 00:38:33 +1000

    Added costs for merits. Will do prereq's later.

--- Rules.rb
+++ Rules.rb
@@ -47,3 +47,55 @@ SKILLS_BY_TYPE = [MENTAL_SKILLS,
                   SOCIAL_SKILLS]
 
 SKILLS = SKILLS_BY_TYPE.flatten
+
+
+# So far I have costs in, but not prerequisites.
+MENTAL_MERITS = [
+  :common_sense               => {:cost => 4,       :prereq => nil},
+  :danger_sense               => {:cost => 2,       :prereq => nil},
+  :eidetic_memory             => {:cost => 2,       :prereq => nil},
+  :encyclopedic_knowledge     => {:cost => 4,       :prereq => nil},
+  :holistic_awareness         => {:cost => 3,       :prereq => nil},
+  :language                   => {:cost => [1..3],  :prereq => nil},
+  :meditative_mind            => {:cost => 1,       :prereq => nil},
+  :unseen_sense               => {:cost => 3,       :prereq => nil}
+]
+
+PHYSICAL_MERITS = [
+  :ambidextrous               => {:cost => 3,       :prereq => nil},
+  :brawling_dodge             => {:cost => 1,       :prereq => nil},
+  :direction_sense            => {:cost => 1,       :prereq => nil},
+  :disarm                     => {:cost => 2,       :prereq => nil},
+  :fast_reflexes              => {:cost => [1..2],  :prereq => nil},
+  :fighting_finesse           => {:cost => 2,       :prereq => nil},
+  :fighting_style_boxing      => {:cost => [1..5],  :prereq => nil},
+  :fighting_style_kung_fu     => {:cost => [1..5],  :prereq => nil},
+  :fighting_style_two_weapons => {:cost => [1..4],  :prereq => nil},
+  :fleet_of_foot              => {:cost => [1..3],  :prereq => nil},
+  :fresh_start                => {:cost => 1,       :prereq => nil},
+  :giant                      => {:cost => 4,       :prereq => nil},
+  :gunslinger                 => {:cost => 3,       :prereq => nil},
+  :iron_stamina               => {:cost => [1..3],  :prereq => nil},
+  :iron_stomach               => {:cost => 2,       :prereq => nil},
+  :natural_immunity           => {:cost => 1,       :prereq => nil},
+  :quick_draw                 => {:cost => 1,       :prereq => nil},
+  :quick_healer               => {:cost => 4,       :prereq => nil},
+  :strong_back                => {:cost => 1,       :prereq => nil},
+  :strong_lungs               => {:cost => 3,       :prereq => nil},
+  :stunt_driver               => {:cost => 3,       :prereq => nil},
+  :toxin_resistance           => {:cost => 2,       :prereq => nil},
+  :weaponry_dodge             => {:cost => 1,       :prereq => nil}
+]
+
+SOCIAL_MERITS = [
+  :allies                     => {:cost => [1..5],  :prereq => nil},
+  :barfly                     => {:cost => 1,       :prereq => nil},
+  :contacts                   => {:cost => [1..5],  :prereq => nil},
+  :fame                       => {:cost => [1..3],  :prereq => nil},
+  :inspiring                  => {:cost => 4,       :prereq => nil},
+  :mentor                     => {:cost => [1..5],  :prereq => nil},
+  :resources                  => {:cost => [1..5],  :prereq => nil},
+  :retainer                   => {:cost => [1..5],  :prereq => nil},
+  :status                     => {:cost => [1..5],  :prereq => nil},
+  :striking_looks             => {:cost => [2, 4],  :prereq => nil}
+]
