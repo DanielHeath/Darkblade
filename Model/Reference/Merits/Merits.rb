4d7fa9a92bf2272778a60514661a4e55e1cba45a
Daniel Heath <daniel.r.heath@gmail.com>
2009-08-23 20:00:41 +1000

    Refactored, now sorts the reference list of merits with rarely used ones at the end.

--- Model/Reference/Merits/Merits.rb
+++ Model/Reference/Merits/Merits.rb
@@ -3,11 +3,12 @@ module Reference; end;
 class Reference::Merit
   attr_reader :name, :type, :costs, :prereqs
   
+  # We only want to sort the list once.
+  @@merits_sorted = false
+  
   def self.merits
-    if not @@merits then
-      # Creates the list of merits on-demand
-      load 'DSL.rb'
-    end
+    load_merits
+    sort_merits
     @@merits
   end
   
@@ -20,4 +21,25 @@ class Reference::Merit
   def add_prerequisite(prereq)
     @prereqs.push prereq
   end
+  
+  private
+  
+  def self.load_merits
+    if not @@merits then
+      # Creates the list of merits on-demand
+      load 'DSL.rb'
+    end
+  end
+  
+  def self.sort_merits
+    if not @@merits_sorted then
+      # Lets put all the ones ending in a number on the end.
+      # Makes it a little easier on the eyes.
+      @@merits_sorted = true
+      extras = @@merits.select {|m| m.name[-1, 1].to_i > 0 }
+      @@merits = @@merits - extras
+      @@merits = @@merits + extras
+    end
+  end
+  
 end
